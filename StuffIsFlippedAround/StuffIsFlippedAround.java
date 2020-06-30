8776f26a19974911282c4436e5086720b41af115
Gabriel Cruz <8129788+gmelodie@users.noreply.github.com>
2020-06-30 19:18:09 +0200

    Change `flag` to `mlh` (official flag format)

--- StuffIsFlippedAround/StuffIsFlippedAround.java
+++ StuffIsFlippedAround/StuffIsFlippedAround.java
@@ -2,7 +2,7 @@ public StuffIsFlippedAround {
 	
 	public static void main(String[] args) {
 		
-		}354209853240-98352408-94523yrt_ecin_galf_a_ton_si_siht{galf
+		}354209853240-98352408-94523yrt_ecin_galf_a_ton_si_siht{hlm
 		
 	}
 	
