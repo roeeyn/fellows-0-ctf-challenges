299f7c7c12a99f22dbf763f6ee8de11c4f99f9b6
Larson Carter <larsoncarter@blazermail.net>
2020-07-07 15:54:43 -0500

    I bet you can't vue this
    This is a very basic challenge where the user simply goes into the vue file and changes the text color from white to black.

---
+++ ibetyoucannotvuethis/src/views/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="about">
+    <h1>mlh{youGuessesWrong}</h1>
+  </div>
+</template>
+
+<style scoped>
+
+h1 {
+  color: white;
+}
+
+</style>
---
+++ ibetyoucannotvuethis/src/views/EntryPointView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <EntryPointComponent/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EntryPointComponent from '@/components/EntryPointComponent.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    EntryPointComponent
+  }
+}
+</script>
