95ceb7262a83d6d2bdb1494938c55776376ea7c5
Gabriel Cruz <gabs.oficial98@gmail.com>
2020-07-20 16:03:49 +0200

    Add CLI arguments

--- warmup/3-express-your-api/solution.py
+++ warmup/3-express-your-api/solution.py
@@ -1,10 +1,17 @@
 import requests
+import sys
 
 
 if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print('usage: python3', sys.argv[0], 'host port')
+        exit(1)
+
     text = ""
 
     while not text.startswith("mlh"):
-        text = requests.get('http://localhost').content.decode()
+        text = requests.get('http://' + sys.argv[1] + ':' +\
+                            sys.argv[2]).content.decode()
+        print(text)
 
     print(text)
