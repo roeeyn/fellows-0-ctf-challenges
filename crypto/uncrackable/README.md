fadd3d0d6fe346fc761621702ff43e4e74810801
Gabriel Cruz <gabs.oficial98@gmail.com>
2020-07-23 11:52:55 +0200

    Add uncrackable chall

---
+++ crypto/uncrackable/README.md
@@ -0,0 +1,16 @@
+# the_most_uncrackable_crypto_ever_yeah_believe_me
+
+Gabriel is always complaining about the current state of the
+art cryptography algorithms. He decided to create a flawless
+encryption algorithm that nobody will ever crack. People
+say his ciphers have "perfect secrecy". Well, mathematically-proven
+perfect ciphers can never have security issues can they.
+
+`DBsNCBZdEC8SG0MCAxY7E0RRHwMUOhhQF0AoFkIREygOQBYeOhQUF0AK`
+
+**Hint**: What good is a cipher if you don't hide your keys?
+
+**Obs (ctf crew)**: Give the file `uncrackable.py` for contestants
+**Flag**: `mlh{y0u_sh0uld_r34lly_h1d3_y0ur_k3ys_dud3}`
+**Solution**: Decode the given ciphertext with `python3 solution.py <CIPHERTEXT>`
+
---
+++ crypto/uncrackable/uncrackable.py
@@ -0,0 +1,28 @@
+import sys
+import base64
+from itertools import cycle
+
+def the_most_uncrackable_crypto_ever_yeah_believe_me(data, key = 'awesomepassword', encode_output=False, decode_input=False):
+
+    if decode_input:
+        data = base64.b64decode(data).decode()
+
+    super_duper_secure_ciphertext = ''.join(chr(ord(x) ^ ord(y)) for (x,y) in zip(data, cycle(key)))
+
+    if encode_output:
+        return base64.b64encode(super_duper_secure_ciphertext.encode()).decode()
+
+    return super_duper_secure_ciphertext
+
+
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("usage: python3", sys.argv[0], "<PLAINTEXT>")
+        exit(1)
+
+    plaintext = sys.argv[1]
+
+    ciphertext = the_most_uncrackable_crypto_ever_yeah_believe_me(plaintext, encode_output=True)
+    print("Ciphertext: ", ciphertext)
